57eee93b644a8a43e8c1c21ce0ebbe210a450179
NkPlast <156546098+NkPlast@users.noreply.github.com>
2024-05-27 14:09:26 +0300

    Add files via upload

---
+++ hw_09_01.py
@@ -0,0 +1,37 @@
+def find_coins_greedy(amount):
+    coins = [50, 25, 10, 5, 2, 1]
+    result = {}
+    for coin in coins:
+        if amount >= coin:
+            count = amount // coin
+            amount -= count * coin
+            result[coin] = count
+    return result
+
+def find_min_coins(amount):
+    coins = [50, 25, 10, 5, 2, 1]
+    dp = [float('inf')] * (amount + 1)
+    dp[0] = 0
+    coin_used = [0] * (amount + 1)
+
+    for coin in coins:
+        for x in range(coin, amount + 1):
+            if dp[x - coin] + 1 < dp[x]:
+                dp[x] = dp[x - coin] + 1
+                coin_used[x] = coin
+
+    result = {}
+    while amount > 0:
+        coin = coin_used[amount]
+        if coin in result:
+            result[coin] += 1
+        else:
+            result[coin] = 1
+        amount -= coin
+
+    return result
+
+# Тестування функцій
+amount = 113
+print("Результат жадібного алгоритму:", find_coins_greedy(amount))
+print("Результат алгоритму динамічного програмування:", find_min_coins(amount))
